67d2a7cf866a32648f2dcf2cfd41ce5e759304ad
florinhegedus <florinxhgds@gmail.com>
2024-11-16 01:54:11 +0200

    revert cors allow all

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 FRONTEND_URL = os.getenv('ALLOWED_HOST')
-ALLOWED_HOSTS = [FRONTEND_URL, 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = [FRONTEND_URL, 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True  # only set True to check problem is not from cors
+CORS_ALLOW_ALL_ORIGINS = False  # only set True to check problem is not from cors
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
