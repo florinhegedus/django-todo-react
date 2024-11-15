7e132d15fc5d33eb1508e157fe4ee459616c54e3
florinhegedus <florinxhgds@gmail.com>
2024-11-16 01:33:25 +0200

    update to check frontend being disconnected from backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000', // Fallback to proxy in development
+  baseURL: process.env.REACT_APP_BACKEND_URL || '', // Fallback to proxy in development
 });
 
 export default api;
