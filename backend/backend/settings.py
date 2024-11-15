d21f4cfcdfbe519dced561a1efe4cf34fc7e3119
florinhegedus <florinxhgds@gmail.com>
2024-11-16 00:39:16 +0200

    allow all hosts to check functionality - TO BE REVERTED

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!D
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST'), 'localhost']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST'), 'localhost', '*']
 
 # Application definition
 
